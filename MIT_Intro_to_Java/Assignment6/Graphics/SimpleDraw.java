d0e28af0881d20b423d90483c62b456f0fc58741
KevinTudor <KevinTudor.work@gmail.com>
2023-10-16 12:08:06 -0400

    Header Update

--- MIT_Intro_to_Java/Assignment6/Graphics/SimpleDraw.java
+++ MIT_Intro_to_Java/Assignment6/Graphics/SimpleDraw.java
@@ -1,3 +1,12 @@
+/* 
+link: https://ocw.mit.edu/courses/6-092-introduction-to-programming-in-java-january-iap-2010/resources/mit6_092iap10_assn06/
+
+Assignment 6: 
+    Explore separating this functionality into two classes. This way, one class can handle moving
+any drawable object around the screen, and it can move any kind of object that we define in another class, which sill
+simply draw itself on the screen. 
+*/
+
 package Assignment6.Graphics;
 
 import java.awt.Color;
