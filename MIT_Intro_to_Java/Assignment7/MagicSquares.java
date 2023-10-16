d94e401e1d6a5c4bfefbbd3863848934ad9be8a5
KevinTudor <KevinTudor.work@gmail.com>
2023-10-16 16:23:04 -0400

    Column Sum Completed

--- MIT_Intro_to_Java/Assignment7/MagicSquares.java
+++ MIT_Intro_to_Java/Assignment7/MagicSquares.java
@@ -18,7 +18,10 @@ public class MagicSquares {
 
     public static boolean testMagic(String pathName) throws IOException{
         // Declare variable to check if all sums are equal
-        List<Integer> sumList = new ArrayList<>();
+        List<Integer> rowSums = new ArrayList<>();
+        List<Integer> columnSums = new ArrayList<>();
+        List<List<Integer>> table = new ArrayList<>();
+
 
         // Open file
         File myObj = new File(pathName);
@@ -29,26 +32,47 @@ public class MagicSquares {
         while (myReader.hasNextLine()) {
             String data = myReader.nextLine();
             String[] rowList = data.split("\t");
+            List<Integer> row = new ArrayList<>();
 
             if (data != ""){
                 Integer sum = 0;
                 for (String value: rowList){
+                    row.add(Integer.valueOf(value));
                     sum += Integer.valueOf(value);
                 }
-                sumList.add(sum);
-                System.out.print(data + "  Sum = ");
-                System.out.println(sum);
+                table.add(row);
+                rowSums.add(sum);
+                System.out.print(data + "  Sum = " + sum + "\n");
             }
         }
         myReader.close();
 
-        return sumList.stream().allMatch(integer -> integer/1 == sumList.get(0));
+        // Parse by column
+        for (int row = 0; row < table.size(); row++){
+            for (int column = 0; column < table.get(row).size(); column++){
+                Integer value = table.get(row).get(column);
+                if (row == 0){
+                    columnSums.add(value);
+                }else{
+                    columnSums.set(column, columnSums.get(column) + value);
+                }
+            }
+        }
+        for (Integer value : columnSums){
+            System.out.print(String.format("%-8d", value));
+        }
+        System.out.println("\n");
+
+        Boolean columnsEqual = columnSums.stream().allMatch(integer -> integer/1 == columnSums.get(0));
+        Boolean rowsEqual = rowSums.stream().allMatch(integer -> integer/1 == rowSums.get(0));
+
+        return columnsEqual && rowsEqual;
     }
     
     public static void main(String[] arguments) throws IOException{
-        String[] fileNames = { "MIT_Intro_to_Java\\Assignment7\\Data\\Mercury.txt", "MIT_Intro_to_Java\\Assignment7\\Data\\Luna.txt" };
+    String[] fileNames = { "MIT_Intro_to_Java\\Assignment7\\Data\\Mercury.txt", "MIT_Intro_to_Java\\Assignment7\\Data\\Luna.txt" };
         for (String fileName : fileNames) {
-            System.out.println(fileName + " rows have same sum? " + testMagic(fileName));
+            System.out.println(fileName.substring(35, fileName.length()) + " rows and columns have same sum? " + testMagic(fileName) + "\n");
         }
     }
 } 
